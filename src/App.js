10edffcbb85877fafc01d42f401726df10520eb7
Sami Alaa <alaaalsudani@ac.c4q.nyc>
2020-03-09 05:30:25 -0400

    adding routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Router} from 'react-router-dom'
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Router exact path='/' component={HomePage}/>
+
     </div>
   );
 }
